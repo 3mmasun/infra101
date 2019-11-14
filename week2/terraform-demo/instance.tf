d40d3f519fc602080abb8ccc2145bd3e521855a1
Emma Sun <emma.sun@thoughtworks.com>
2019-11-14 20:29:15 +0800

    add aws_elb and attach to public subnet and target example instance

--- week2/terraform-demo/instance.tf
+++ week2/terraform-demo/instance.tf
@@ -1,17 +1,28 @@
 resource "aws_instance" "example" {
   ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
   instance_type = "t2.micro"
-
   #vpc subnet
   subnet_id = "${aws_subnet.infra_public.id}"
-
   #security group
-  vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
-
+  vpc_security_group_ids = ["${aws_security_group.allow_ssh_http_http.id}"]
   #public SSH key
   key_name = "${aws_key_pair.mykeypair.key_name}"
-
   tags = {
     Name = "HelloInfra101"
+  }
+}
+
+resource "aws_elb" "web" {
+  name = "terraform-example-elb"
+
+  subnets         = ["${aws_subnet.infra_public.id}"]
+  security_groups = ["${aws_security_group.allow_ssh_http.id}"]
+  instances       = ["${aws_instance.example.id}"]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
   }
 }
